4466e5fbd42c9f1d20f5130bbaec5037c78c4699
matiferre15 <matias.ferre@hotmail.com>
2022-03-24 21:13:21 -0300

    histo (incompleto)

--- Tp1-Sim/Histograma.Designer.cs
+++ Tp1-Sim/Histograma.Designer.cs
@@ -29,10 +29,18 @@ namespace Tp1_Sim
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // Histograma
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Name = "Histograma";
             this.Text = "Histograma";
+            this.Load += new System.EventHandler(this.Histograma_Load);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
--- Tp1-Sim/Histograma.cs
+++ Tp1-Sim/Histograma.cs
@@ -5,14 +5,27 @@ using System.Data;
 using System.Drawing;
 using System.Text;
 using System.Windows.Forms;
+using static Tp1_Sim.Form1;
 
 namespace Tp1_Sim
 {
     public partial class Histograma : Form
     {
+        private List<Intervalo> intervalos;
         public Histograma()
         {
             InitializeComponent();
+        }
+
+        public Histograma(List<Intervalo> intervalos)
+        {
+            InitializeComponent();
+
+        }
+
+        private void Histograma_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
