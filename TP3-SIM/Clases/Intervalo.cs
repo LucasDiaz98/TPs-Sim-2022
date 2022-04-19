382f9aff5e65362937bb1df62a064ee536bd400f
@LucasDiaz <ldiaz@encodesa.com.ar>
2022-04-18 21:42:11 -0300

    Primer commit tp3

---
+++ TP3-SIM/Clases/Intervalo.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP3SIM.Clases
+{
+    class Intervalo
+    {
+        private int numIteracion;
+        private double valor_inf;
+        private double valor_sup;
+        private double marca_clase;
+        private int frecuencia_observada;
+        private int frecuencia_esperada;
+        private double frecuencia_relativa;
+        private int frecuencia_acumulada;
+        private double frecuencia_relativa_acumulada;
+
+        public int Num_iteracion { get => numIteracion; set => numIteracion = value; }
+        public double Valor_inf { get => valor_inf; set => valor_inf = value; }
+        public double Valor_sup { get => valor_sup; set => valor_sup = value; }
+        public double Marca_clase { get => marca_clase; set => marca_clase = value; }
+        public int Frecuencia_observada { get => frecuencia_observada; set => frecuencia_observada = value; }
+        public int Frecuencia_acumulada { get => frecuencia_acumulada; set => frecuencia_acumulada = value; }
+        public double Frecuencia_relativa { get => frecuencia_relativa; set => frecuencia_relativa = value; }
+        public double Frecuencia_relativa_acumulada { get => frecuencia_relativa_acumulada; set => frecuencia_relativa_acumulada = value; }
+        public int Frecuencia_esperada { get => frecuencia_esperada; set => frecuencia_esperada = value; }
+
+
+        //Esta funcion se encarga de comprobar que un numero rnd este dentro del intervalo
+        public bool estaEnIntervalo(double rnd)
+        {
+            return (this.valor_inf <= rnd && rnd < this.valor_sup);
+        }
+
+        //Esta funcion toma ambos limites del intervalo para unirlos en un solo string
+        public string intervaloString()
+        {
+            return valor_inf.ToString() + " - " + valor_sup.ToString();
+        }
+    }
+}
---
+++ TP3-SIM/Form1.Designer.cs
@@ -0,0 +1,215 @@
+﻿
+namespace TP3SIM
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtA = new System.Windows.Forms.TextBox();
+            this.txtB = new System.Windows.Forms.TextBox();
+            this.txtN = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnGenerar = new System.Windows.Forms.Button();
+            this.cmbDistribucion = new System.Windows.Forms.ComboBox();
+            this.txtLambda = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtMedia = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtDesvEstandar = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // txtA
+            // 
+            this.txtA.Location = new System.Drawing.Point(176, 103);
+            this.txtA.Name = "txtA";
+            this.txtA.Size = new System.Drawing.Size(100, 20);
+            this.txtA.TabIndex = 0;
+            this.txtA.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtA_KeyPress);
+            // 
+            // txtB
+            // 
+            this.txtB.Location = new System.Drawing.Point(176, 139);
+            this.txtB.Name = "txtB";
+            this.txtB.Size = new System.Drawing.Size(100, 20);
+            this.txtB.TabIndex = 1;
+            this.txtB.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtB_KeyPress);
+            // 
+            // txtN
+            // 
+            this.txtN.Location = new System.Drawing.Point(176, 65);
+            this.txtN.Name = "txtN";
+            this.txtN.Size = new System.Drawing.Size(100, 20);
+            this.txtN.TabIndex = 2;
+            this.txtN.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtN_KeyPress);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(73, 68);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(95, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "numeros a generar";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(73, 106);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(13, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "a";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(73, 142);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(13, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "b";
+            // 
+            // btnGenerar
+            // 
+            this.btnGenerar.Location = new System.Drawing.Point(160, 303);
+            this.btnGenerar.Name = "btnGenerar";
+            this.btnGenerar.Size = new System.Drawing.Size(75, 23);
+            this.btnGenerar.TabIndex = 6;
+            this.btnGenerar.Text = "generar";
+            this.btnGenerar.UseVisualStyleBackColor = true;
+            this.btnGenerar.Click += new System.EventHandler(this.btnGenerar_Click);
+            // 
+            // cmbDistribucion
+            // 
+            this.cmbDistribucion.FormattingEnabled = true;
+            this.cmbDistribucion.Items.AddRange(new object[] {
+            "Uniforme A-B",
+            "Exponencial negativa",
+            "Normal",
+            "Poisson"});
+            this.cmbDistribucion.Location = new System.Drawing.Point(76, 24);
+            this.cmbDistribucion.Name = "cmbDistribucion";
+            this.cmbDistribucion.Size = new System.Drawing.Size(121, 21);
+            this.cmbDistribucion.TabIndex = 7;
+            // 
+            // txtLambda
+            // 
+            this.txtLambda.Location = new System.Drawing.Point(176, 176);
+            this.txtLambda.Name = "txtLambda";
+            this.txtLambda.Size = new System.Drawing.Size(100, 20);
+            this.txtLambda.TabIndex = 8;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(73, 183);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(45, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Lambda";
+            // 
+            // txtMedia
+            // 
+            this.txtMedia.Location = new System.Drawing.Point(176, 217);
+            this.txtMedia.Name = "txtMedia";
+            this.txtMedia.Size = new System.Drawing.Size(100, 20);
+            this.txtMedia.TabIndex = 10;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(73, 220);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(36, 13);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Media";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(73, 265);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(77, 13);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "Desv Estandar";
+            // 
+            // txtDesvEstandar
+            // 
+            this.txtDesvEstandar.Location = new System.Drawing.Point(176, 265);
+            this.txtDesvEstandar.Name = "txtDesvEstandar";
+            this.txtDesvEstandar.Size = new System.Drawing.Size(100, 20);
+            this.txtDesvEstandar.TabIndex = 13;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1344, 721);
+            this.Controls.Add(this.txtDesvEstandar);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtMedia);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtLambda);
+            this.Controls.Add(this.cmbDistribucion);
+            this.Controls.Add(this.btnGenerar);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtN);
+            this.Controls.Add(this.txtB);
+            this.Controls.Add(this.txtA);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtA;
+        private System.Windows.Forms.TextBox txtB;
+        private System.Windows.Forms.TextBox txtN;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnGenerar;
+        private System.Windows.Forms.ComboBox cmbDistribucion;
+        private System.Windows.Forms.TextBox txtLambda;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtMedia;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtDesvEstandar;
+    }
+}
+
